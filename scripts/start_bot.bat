07a80b6bd18541e05e7447a0e5d96f421b899b2f
Adam Seidman <adamjseidman@gmail.com>
2024-09-09 21:04:18 -0400

    Create start_bot.bat

---
+++ scripts/start_bot.bat
@@ -0,0 +1,28 @@
+@ECHO OFF
+CD ..
+
+IF EXIST tempt.txt DEL /F tempt.txt
+TASKLIST /NH /FI "WINDOWTITLE EQ Spotify Guesser Bot" > temp.txt
+SET /P titlequery=<temp.txt
+IF NOT "%titlequery%" == "INFO: No tasks are running which match the specified criteria." (GOTO HasInst)
+
+:CreateBot
+ECHO.
+ECHO Initializing...
+ECHO.
+TITLE Spotify Guesser Bot
+NODE .
+ECHO Program will now restart...
+ECHO.
+GOTO CreateBot
+
+:HasInst
+ECHO There is already an instance of the bot running.
+ECHO Exit now to prevent replacing that instance.
+ECHO.
+PAUSE
+
+TASKKILL /F /FI "WINDOWTITLE EQ Spotify Guesser Bot"
+PAUSE
+CLS
+GOTO CreateBot
